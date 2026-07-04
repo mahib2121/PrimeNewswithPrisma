ec812073fa996949b0bb5ae1149b9f80387a0486
mahib2121 <khanmahibalam@gmail.com>
2026-07-04 23:18:56 +0600

    Updated System for user  registration

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,10 @@ import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import config from "./config";
+import { userRoutes } from "./module/users/user.route";
 const app: Application = express();
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
-});
+
+app.get("/api/user");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -15,4 +15,11 @@ app.use(
     credentials: true,
   }),
 );
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, World!");
+});
+
+app.use("/api/users", userRoutes);
+
 export default app;
---
+++ src/module/users/user.controller.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+import { userService } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+
+const registerUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const payload = req.body;
+    const user = await userService.registerUserIntoDB(payload);
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "User registered successfully",
+      data: { user },
+    });
+  },
+);
+
+export const userController = {
+  registerUser,
+};
